martin-schwenke/nfs-ganesha

commit 7b1ff6e00d095f1fceb0f56243ac0ad365e31d9c

9P: Check file open mode for locks
Change-Id: If810a9625e2db9f0e9322c56b227cb247cfb053e
Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

src/Protocols/9P/9p_lock.c
@@ -76,6 +76,7 @@ int _9p_lock(struct _9p_request_data *req9p, u32 *plenout, char *preply)
 	u32 *proc_id = NULL;
 	u16 *client_id_len = NULL;
 	char *client_id_str = NULL;
+	fsal_openflags_t openflags;
 
 	u8 status = _9P_LOCK_SUCCESS;
 
@@ -163,7 +164,23 @@ int _9p_lock(struct _9p_request_data *req9p, u32 *plenout, char *preply)
 			status = _9P_LOCK_GRACE;
 			break;
 		}
+
 		STATELOCK_lock(pfid->pentry);
+
+		/* Test that the file is open in the right mode for the lock */
+		openflags = pfid->pentry->obj_ops->status2(pfid->pentry,
+							   pfid->state);
+
+		if ((*type == _9P_LOCK_TYPE_RDLCK &&
+					((openflags & FSAL_O_READ) == 0)) ||
+		    (*type == _9P_LOCK_TYPE_WRLCK &&
+					((openflags & FSAL_O_WRITE) == 0))) {
+			/* Open in wrong mode - return error */
+			STATELOCK_unlock(pfid->pentry);
+			status = _9P_LOCK_ERROR;
+			break;
+		}
+
 		state_status = state_lock(pfid->pentry, powner, pfid->state,
 					  STATE_NON_BLOCKING, NULL, &lock,
 					  NULL, NULL);

commit b3ec8cc0af0343d1430bb891517e7624e7fffaea

9P: Avoid grace reference counting bug
Prior code in this case increments the no-grace reference count via
nfs_get_grace_status(false).  An early break from this case also needs
to decrement it.

This is a regression introduced by commit
7b1ff6e00d095f1fceb0f56243ac0ad365e31d9c.

Reported-by: Arnab Tah <atah@ddn.com>
Signed-off-by: Martin Schwenke <mschwenke@ddn.com>
Change-Id: If7f3a6a663d89886cdab1a28fc8b269760084899

src/Protocols/9P/9p_lock.c
@@ -177,6 +177,7 @@ int _9p_lock(struct _9p_request_data *req9p, u32 *plenout, char *preply)
 					((openflags & FSAL_O_WRITE) == 0))) {
 			/* Open in wrong mode - return error */
 			STATELOCK_unlock(pfid->pentry);
+			nfs_put_grace_status();
 			status = _9P_LOCK_ERROR;
 			break;
 		}